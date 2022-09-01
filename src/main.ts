0c878a8cbd42fb2f2b94ff575bd8ba577c270f15
renatormsilva <renato.brtkm@gmail.com>
2022-09-01 09:43:53 -0400

    feat: eneable cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.use((req, res, next) => {
 
     if (req.method == 'DELETE') {
