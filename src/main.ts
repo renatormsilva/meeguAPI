526510a0a1ea3d89e3419e05830b83a469894a64
renatormsilva <renato.brtkm@gmail.com>
2022-09-01 09:26:30 -0400

    feat: add cors

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+const cors = require('cors')
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use((req, res, next) => {
+    
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Headers", "*");
+      res.header("Access-Control-Allow-Private-Network", true);
+      res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+      app.use(cors());
+      next();
+    
     if (req.method == 'DELETE') {
       console.log('salve');
       const token = req.headers['x-access-token'];
