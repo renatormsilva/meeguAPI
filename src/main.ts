42f2f87340d7c2c217b170b4804022806521356f
renatormsilva <renato.brtkm@gmail.com>
2022-09-01 08:36:43 -0400

    feat: initial configs

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.use((req, res, next) => {
+    if (req.method == 'DELETE') {
+      console.log('salve');
+      const token = req.headers['x-access-token'];
+      if (token != process.env.SECRET) {
+        return res
+          .status(500)
+          .json({ auth: false, message: 'Failed to authenticate token.' });
+      }
+    }
+    next();
+  });
+
+  await app.listen(process.env.PORT);
+}
+bootstrap();
